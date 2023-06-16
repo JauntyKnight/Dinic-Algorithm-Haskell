14dfbc94b07b0cebfeeae74cf6cb1fd1b5664c02
Andrei Lupasco <lupascoandrei3@gmail.com>
2023-06-16 17:54:39 +0200

    Used hindent to apply Haskell style to the code

--- Dinic.hs
+++ Dinic.hs
@@ -1,67 +1,111 @@
 -- Dinic's Algorithm
-
 module Dinic where
 
 import FlowGraph
 
+import qualified Data.IntMap.Strict as Map
+  ( IntMap
+  , (!)
+  , assocs
+  , empty
+  , fromList
+  , insert
+  , member
+  , notMember
+  , null
+  , singleton
+  ) -- for a Dictionary implementation
 import Data.Maybe
-import qualified Data.Sequence as Seq (Seq, null, singleton, (|>), index, drop) -- for a Queue implementation
-import qualified Data.IntMap.Strict as Map (IntMap, singleton, null, member, notMember, insert, assocs, (!), fromList, empty) -- for a Dictionary implementation
-import Debug.Trace
+import qualified Data.Sequence as Seq (Seq, (|>), drop, index, null, singleton) -- for a Queue implementation
 
 type LayeredStructure = Map.IntMap Int
 
 -- Assign layers to the vertices of the FlowGraph in a bfs manner
 -- Returns: a dictionary mapping each vertex to its layer
 layers :: FlowGraph -> Vertex -> LayeredStructure
-layers g s = layers' g s (Seq.singleton (s, 0)) (Map.singleton s 0) where
+layers g s = layers' g s (Seq.singleton (s, 0)) (Map.singleton s 0)
+  where
     layers' g s q m
-        | Seq.null q = m                    -- no more vertices to visit, base case
-        | otherwise = layers' g s q'' m'' where    -- recursive case: take the top of the queue, visit its neighbors, and add them to the queue
-            (v, d) = Seq.index q 0          -- get the top of the queue
-            q' = Seq.drop 1 q               -- remove the top of the queue
+      | Seq.null q = m -- no more vertices to visit, base case
+      | otherwise = layers' g s q'' m'' -- recursive case: take the top of the queue, visit its neighbors, and add them to the queue
+      where
+        (v, d) = Seq.index q 0 -- get the top of the queue
+        q' = Seq.drop 1 q -- remove the top of the queue
             -- add the neighbors of v to the queue if they are not already in the dictionary
-            (q'', m'') = foldl (\(q, m) (u, c) -> if Map.member u m || c <= 0 then (q, m) else (q Seq.|> (u, d + 1), Map.insert u (d+1) m)) (q', m) (neighbors g v)
+        (q'', m'') =
+          foldl
+            (\(q, m) (u, c) ->
+               if Map.member u m || c <= 0
+                 then (q, m)
+                 else (q Seq.|> (u, d + 1), Map.insert u (d + 1) m))
+            (q', m)
+            (neighbors g v)
 
 -- Construct the layered FlowGraph
 -- i.e. keep only the edges that go from a vertex of layer i to a vertex of layer i + 1
 -- Complexity: O(V + E)
-layeredFlowGraph :: FlowGraph -> Vertex -> Vertex -> LayeredStructure -> FlowGraph
-layeredFlowGraph g s t layers_ = Map.fromList [(v, Map.fromList $ filter (\(u, c) -> Map.member u layers_  && (layers_ Map.! u == d + 1)) (neighbors g v)) | (v, d) <- Map.assocs layers_, d <= max_depth]
-    where max_depth = layers_ Map.! t
+layeredFlowGraph ::
+     FlowGraph -> Vertex -> Vertex -> LayeredStructure -> FlowGraph
+layeredFlowGraph g s t layers_ =
+  Map.fromList
+    [ ( v
+      , Map.fromList $
+        filter
+          (\(u, c) -> Map.member u layers_ && (layers_ Map.! u == d + 1))
+          (neighbors g v))
+    | (v, d) <- Map.assocs layers_
+    , d <= max_depth
+    ]
+  where
+    max_depth = layers_ Map.! t
 
 -- Find an augmenting path in the layered FlowGraph in a DFS manner
 -- Returns: a path from s to t in the layered FlowGraph
 findBlocking :: FlowGraph -> LayeredStructure -> Vertex -> Vertex -> Maybe Path
-findBlocking g layers_ s' t = reverse <$> restorePath (findBlocking' g layers_ t [s'] Map.empty) s' t where
-        max_depth = layers_ Map.! t
-        findBlocking' g layers_ t stack parents
-            | null stack = parents
-            | s == t = parents
-            | layers_ Map.! s >= max_depth = findBlocking' g layers_ t (tail stack) parents  -- no need to explore this vertex
-            | otherwise = findBlocking' g layers_ t stack' parents' where
-                s = head stack
-                nbs = [v | (v, c) <- neighbors g s, c > 0, Map.notMember v parents]
-                stack' = nbs ++ tail stack
-                parents' = foldl (\parents v -> Map.insert v s parents) parents nbs
+findBlocking g layers_ s' t =
+  reverse <$> restorePath (findBlocking' g layers_ t [s'] Map.empty) s' t
+  where
+    max_depth = layers_ Map.! t
+    findBlocking' g layers_ t stack parents
+      | null stack = parents
+      | s == t = parents
+      | layers_ Map.! s >= max_depth =
+        findBlocking' g layers_ t (tail stack) parents -- no need to explore this vertex
+      | otherwise = findBlocking' g layers_ t stack' parents'
+      where
+        s = head stack
+        nbs = [v | (v, c) <- neighbors g s, c > 0, Map.notMember v parents]
+        stack' = nbs ++ tail stack
+        parents' = foldl (\parents v -> Map.insert v s parents) parents nbs
 
-
-pushAugmenting :: FlowGraph -> Path -> (FlowGraph -> Vertex -> Vertex -> Flow -> FlowGraph) -> (Flow, FlowGraph)
-pushAugmenting g p pushFlow_ = (f, foldl (\g (u, v) -> pushFlow_ g u v f) g edges) where
+pushAugmenting ::
+     FlowGraph
+  -> Path
+  -> (FlowGraph -> Vertex -> Vertex -> Flow -> FlowGraph)
+  -> (Flow, FlowGraph)
+pushAugmenting g p pushFlow_ =
+  (f, foldl (\g (u, v) -> pushFlow_ g u v f) g edges)
+  where
     edges = zip p (tail p)
     f = minimum [c | (u, v) <- edges, let (_, _, c) = getEdge g u v]
 
-
-pushWhilePossible :: FlowGraph -> FlowGraph -> LayeredStructure -> Vertex -> Vertex -> (Flow, FlowGraph, FlowGraph)
-pushWhilePossible g layered_g layers_ s t = case augmenting_path of
+pushWhilePossible ::
+     FlowGraph
+  -> FlowGraph
+  -> LayeredStructure
+  -> Vertex
+  -> Vertex
+  -> (Flow, FlowGraph, FlowGraph)
+pushWhilePossible g layered_g layers_ s t =
+  case augmenting_path of
     Nothing -> (0, g, layered_g)
     Just p -> (f + f'', g'', layered_g'')
-    where augmenting_path = findBlocking layered_g layers_ s t
-          Just path = augmenting_path
-          (f, g') = pushAugmenting g path pushFlow
-          (_, layered_g') = pushAugmenting layered_g path pushFlowLayered
-          (f'', g'', layered_g'') = pushWhilePossible g' layered_g' layers_ s t
-
+  where
+    augmenting_path = findBlocking layered_g layers_ s t
+    Just path = augmenting_path
+    (f, g') = pushAugmenting g path pushFlow
+    (_, layered_g') = pushAugmenting layered_g path pushFlowLayered
+    (f'', g'', layered_g'') = pushWhilePossible g' layered_g' layers_ s t
 
 -- | Dinic Algorithm
 --   Returns: the maximum flow and the final residual graph.
@@ -69,21 +113,30 @@ pushWhilePossible g layered_g layers_ s t = case augmenting_path of
 --   Complexity: O(V^2 E)
 dinic :: FlowGraph -> Vertex -> Vertex -> (Flow, FlowGraph)
 dinic g s t
-    | Map.null layers_ || not (Map.member t layers_) = (0, g)  -- t is not reachable from s
-    | otherwise = (f' + f'', g'')
-    where layers_ = layers g s
-          layered_g = layeredFlowGraph g s t layers_
-          (f', g', _) = pushWhilePossible g layered_g layers_ s t
-          (f'', g'') = dinic g' s t
-
-
+  | Map.null layers_ || not (Map.member t layers_) = (0, g) -- t is not reachable from s
+  | otherwise = (f' + f'', g'')
+  where
+    layers_ = layers g s
+    layered_g = layeredFlowGraph g s t layers_
+    (f', g', _) = pushWhilePossible g layered_g layers_ s t
+    (f'', g'') = dinic g' s t
 
 -- | Dinic but it returns the flow over each edge
 --   Thus it needs to know the original list of edges of the graph
 --   Returns: the maximum flow and a list of edges with their flow
 --   Complexity: O(V^2 E) Same as the Dinic above
-dinicWithEdgesFlow :: FlowGraph -> [Edge] -> Vertex -> Vertex -> (Flow, [(Vertex, Vertex, Capacity, Flow)])
-dinicWithEdgesFlow g edges s t = (flow, edges_flow) where
+dinicWithEdgesFlow ::
+     FlowGraph
+  -> [Edge]
+  -> Vertex
+  -> Vertex
+  -> (Flow, [(Vertex, Vertex, Capacity, Flow)])
+dinicWithEdgesFlow g edges s t = (flow, edges_flow)
+  where
     (flow, g') = dinic g s t
-    edges_flow = [(u, v, c, c - f) | (u, v, c) <- edges, let (_, _, f) = tryGetEdge g' u v]
-    tryGetEdge g u v = if Map.member u g && Map.member v (g Map.! u) then getEdge g u v else (u, v, 0)
+    edges_flow =
+      [(u, v, c, c - f) | (u, v, c) <- edges, let (_, _, f) = tryGetEdge g' u v]
+    tryGetEdge g u v =
+      if Map.member u g && Map.member v (g Map.! u)
+        then getEdge g u v
+        else (u, v, 0)
--- FlowGraph.hs
+++ FlowGraph.hs
@@ -1,80 +1,89 @@
 module FlowGraph where
 
-import Data.Array as Array
-import qualified Data.IntMap.Strict as Map
 import Data.IntMap (IntMap)
+import qualified Data.IntMap.Strict as Map
 import Data.List
-import Debug.Trace
 import System.IO
 
 type Vertex = Int
-type VertexMap a = Map.IntMap a
-type Capacity = Int
-type Flow = Int
-type Edge = (Vertex, Vertex, Capacity)
-type Path = [Vertex]
-type FlowGraph = VertexMap (VertexMap Capacity)
 
+type VertexMap a = Map.IntMap a
+
+type Capacity = Int
+
+type Flow = Int
+
+type Edge = (Vertex, Vertex, Capacity)
+
+type Path = [Vertex]
+
+type FlowGraph = VertexMap (VertexMap Capacity)
 
 fromAssocList :: [(Vertex, [(Vertex, Capacity)])] -> FlowGraph
 fromAssocList assocl = Map.fromList [(v, Map.fromList nbs) | (v, nbs) <- assocl]
 
-
 fromEdgeList :: [Edge] -> FlowGraph
-fromEdgeList edges = fromAssocList assocl where
-    grouped = groupBy (\(u, _, _) (v, _, _) -> u == v) $ sortBy (\(u, _, _) (v, _, _) -> compare u v) edges
+fromEdgeList edges = fromAssocList assocl
+  where
+    grouped =
+      groupBy (\(u, _, _) (v, _, _) -> u == v) $
+      sortBy (\(u, _, _) (v, _, _) -> compare u v) edges
     fst3 (x, _, _) = x
-    assocl = [(v, nbs) | group <- grouped, let v = fst3 $ head group, let nbs = map (\(_, w, c) -> (w, c)) group]
-
+    assocl =
+      [ (v, nbs)
+      | group <- grouped
+      , let v = fst3 $ head group
+      , let nbs = map (\(_, w, c) -> (w, c)) group
+      ]
 
 toEdgeList :: FlowGraph -> [Edge]
 toEdgeList g = [(u, v, c) | (u, nbs) <- Map.assocs g, (v, c) <- Map.assocs nbs]
 
 getEdge :: FlowGraph -> Vertex -> Vertex -> Edge
-getEdge g u v = (u, v, c) where
+getEdge g u v = (u, v, c)
+  where
     c = (g Map.! u) Map.! v
-
 
 neighbors :: FlowGraph -> Vertex -> [(Vertex, Capacity)]
 neighbors g v
-    | Map.member v g = Map.assocs (g Map.! v)
-    | otherwise = []
-
+  | Map.member v g = Map.assocs (g Map.! v)
+  | otherwise = []
 
 pushFlow :: FlowGraph -> Vertex -> Vertex -> Flow -> FlowGraph
-pushFlow g u v f = Map.adjust (Map.adjust (subtract f) v) u g' where
+pushFlow g u v f = Map.adjust (Map.adjust (subtract f) v) u g'
+  where
     g' = Map.adjust (Map.adjust (+ f) u) v g
-
 
 pushFlowLayered :: FlowGraph -> Vertex -> Vertex -> Flow -> FlowGraph
 pushFlowLayered g u v f = Map.adjust (Map.adjust (\c -> c - f) v) u g
 
-
 -- Assumes there are no cycles of size 2, otherwise it may yield a wrong result
 constructResidual :: FlowGraph -> FlowGraph
-constructResidual g = fromEdgeList $ edges ++ map (\(u, v, c) -> (v, u, 0)) edges where
+constructResidual g =
+  fromEdgeList $ edges ++ map (\(u, v, c) -> (v, u, 0)) edges
+  where
     edges = toEdgeList g
-
 
 removeEdge :: FlowGraph -> Vertex -> Vertex -> FlowGraph
 removeEdge g u v = Map.adjust (Map.delete v) u g
 
 restorePath :: VertexMap Vertex -> Vertex -> Vertex -> Maybe Path
 restorePath parents s t
-    | s == t = Just [s]
-    | not $ Map.member t parents = Nothing
-    | otherwise = (t:) <$> restorePath parents s (parents Map.! t)
+  | s == t = Just [s]
+  | not $ Map.member t parents = Nothing
+  | otherwise = (t :) <$> restorePath parents s (parents Map.! t)
 
 -- reads a FlowGraph from a file in the format:
 -- n s t -- number of edges, source and sink on the first line
 -- u v c -- edge from u to v with capacity c on the remaining lines
 readFileEdgeList :: String -> IO (Vertex, Vertex, [Edge])
 readFileEdgeList filename = do
-        handle <- openFile filename ReadMode
-        contents <- hGetContents handle
-        let ls = words contents
-            [n, s, t] = map read $ take 3 ls
-            edges = map (\(u, v, c) -> (read u, read v, read c)) . group3 $ drop 3 ls
-        return (s, t, edges)
-        where group3 [] = []
-              group3 (x:y:z:xs) = (x, y, z) : group3 xs
+  handle <- openFile filename ReadMode
+  contents <- hGetContents handle
+  let ls = words contents
+      [n, s, t] = map read $ take 3 ls
+      edges = map (\(u, v, c) -> (read u, read v, read c)) . group3 $ drop 3 ls
+  return (s, t, edges)
+  where
+    group3 [] = []
+    group3 (x:y:z:xs) = (x, y, z) : group3 xs
