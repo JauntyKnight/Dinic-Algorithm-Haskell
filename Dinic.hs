2f8bea2f678215125f1fe42d90349b88da4004dc
Andrei Lupasco <lupascoandrei3@gmail.com>
2023-06-16 17:46:28 +0200

    Added version of Dinic which outputs the flow over each edge

--- Dinic.hs
+++ Dinic.hs
@@ -11,9 +11,9 @@ import Debug.Trace
 
 type LayeredStructure = Map.IntMap Int
 
--- Assign layers to the vertices of the graph in a bfs manner
+-- Assign layers to the vertices of the FlowGraph in a bfs manner
 -- Returns: a dictionary mapping each vertex to its layer
-layers :: Graph -> Vertex -> LayeredStructure
+layers :: FlowGraph -> Vertex -> LayeredStructure
 layers g s = layers' g s (Seq.singleton (s, 0)) (Map.singleton s 0) where
     layers' g s q m
         | Seq.null q = m                    -- no more vertices to visit, base case
@@ -23,16 +23,16 @@ layers g s = layers' g s (Seq.singleton (s, 0)) (Map.singleton s 0) where
             -- add the neighbors of v to the queue if they are not already in the dictionary
             (q'', m'') = foldl (\(q, m) (u, c) -> if Map.member u m || c <= 0 then (q, m) else (q Seq.|> (u, d + 1), Map.insert u (d+1) m)) (q', m) (neighbors g v)
 
--- Construct the layered graph
+-- Construct the layered FlowGraph
 -- i.e. keep only the edges that go from a vertex of layer i to a vertex of layer i + 1
 -- Complexity: O(V + E)
-layeredGraph :: Graph -> Vertex -> Vertex -> LayeredStructure -> Graph
-layeredGraph g s t layers_ = Map.fromList [(v, Map.fromList $ filter (\(u, c) -> Map.member u layers_  && (layers_ Map.! u == d + 1)) (neighbors g v)) | (v, d) <- Map.assocs layers_, d <= max_depth]
+layeredFlowGraph :: FlowGraph -> Vertex -> Vertex -> LayeredStructure -> FlowGraph
+layeredFlowGraph g s t layers_ = Map.fromList [(v, Map.fromList $ filter (\(u, c) -> Map.member u layers_  && (layers_ Map.! u == d + 1)) (neighbors g v)) | (v, d) <- Map.assocs layers_, d <= max_depth]
     where max_depth = layers_ Map.! t
 
--- Find an augmenting path in the layered graph in a DFS manner
--- Returns: a path from s to t in the layered graph
-findBlocking :: Graph -> LayeredStructure -> Vertex -> Vertex -> Maybe Path
+-- Find an augmenting path in the layered FlowGraph in a DFS manner
+-- Returns: a path from s to t in the layered FlowGraph
+findBlocking :: FlowGraph -> LayeredStructure -> Vertex -> Vertex -> Maybe Path
 findBlocking g layers_ s' t = reverse <$> restorePath (findBlocking' g layers_ t [s'] Map.empty) s' t where
         max_depth = layers_ Map.! t
         findBlocking' g layers_ t stack parents
@@ -46,13 +46,13 @@ findBlocking g layers_ s' t = reverse <$> restorePath (findBlocking' g layers_ t
                 parents' = foldl (\parents v -> Map.insert v s parents) parents nbs
 
 
-pushAugmenting :: Graph -> Path -> (Graph -> Vertex -> Vertex -> Flow -> Graph) -> (Flow, Graph)
+pushAugmenting :: FlowGraph -> Path -> (FlowGraph -> Vertex -> Vertex -> Flow -> FlowGraph) -> (Flow, FlowGraph)
 pushAugmenting g p pushFlow_ = (f, foldl (\g (u, v) -> pushFlow_ g u v f) g edges) where
     edges = zip p (tail p)
     f = minimum [c | (u, v) <- edges, let (_, _, c) = getEdge g u v]
 
 
-pushWhilePossible :: Graph -> Graph -> LayeredStructure -> Vertex -> Vertex -> (Flow, Graph, Graph)
+pushWhilePossible :: FlowGraph -> FlowGraph -> LayeredStructure -> Vertex -> Vertex -> (Flow, FlowGraph, FlowGraph)
 pushWhilePossible g layered_g layers_ s t = case augmenting_path of
     Nothing -> (0, g, layered_g)
     Just p -> (f + f'', g'', layered_g'')
@@ -64,15 +64,26 @@ pushWhilePossible g layered_g layers_ s t = case augmenting_path of
 
 
 -- | Dinic Algorithm
+--   Returns: the maximum flow and the final residual graph.
+--   Pay attention that the residual graph is not the same as the input graph.
 --   Complexity: O(V^2 E)
-dinic :: Graph -> Vertex -> Vertex -> (Flow, Graph)
+dinic :: FlowGraph -> Vertex -> Vertex -> (Flow, FlowGraph)
 dinic g s t
     | Map.null layers_ || not (Map.member t layers_) = (0, g)  -- t is not reachable from s
     | otherwise = (f' + f'', g'')
     where layers_ = layers g s
-          layered_g = layeredGraph g s t layers_
+          layered_g = layeredFlowGraph g s t layers_
           (f', g', _) = pushWhilePossible g layered_g layers_ s t
           (f'', g'') = dinic g' s t
 
 
 
+-- | Dinic but it returns the flow over each edge
+--   Thus it needs to know the original list of edges of the graph
+--   Returns: the maximum flow and a list of edges with their flow
+--   Complexity: O(V^2 E) Same as the Dinic above
+dinicWithEdgesFlow :: FlowGraph -> [Edge] -> Vertex -> Vertex -> (Flow, [(Vertex, Vertex, Capacity, Flow)])
+dinicWithEdgesFlow g edges s t = (flow, edges_flow) where
+    (flow, g') = dinic g s t
+    edges_flow = [(u, v, c, c - f) | (u, v, c) <- edges, let (_, _, f) = tryGetEdge g' u v]
+    tryGetEdge g u v = if Map.member u g && Map.member v (g Map.! u) then getEdge g u v else (u, v, 0)
--- FlowGraph.hs
+++ FlowGraph.hs
@@ -13,50 +13,50 @@ type Capacity = Int
 type Flow = Int
 type Edge = (Vertex, Vertex, Capacity)
 type Path = [Vertex]
-type Graph = VertexMap (VertexMap Capacity)
+type FlowGraph = VertexMap (VertexMap Capacity)
 
 
-fromAssocList :: [(Vertex, [(Vertex, Capacity)])] -> Graph
+fromAssocList :: [(Vertex, [(Vertex, Capacity)])] -> FlowGraph
 fromAssocList assocl = Map.fromList [(v, Map.fromList nbs) | (v, nbs) <- assocl]
 
 
-fromEdgeList :: [Edge] -> Graph
+fromEdgeList :: [Edge] -> FlowGraph
 fromEdgeList edges = fromAssocList assocl where
     grouped = groupBy (\(u, _, _) (v, _, _) -> u == v) $ sortBy (\(u, _, _) (v, _, _) -> compare u v) edges
     fst3 (x, _, _) = x
     assocl = [(v, nbs) | group <- grouped, let v = fst3 $ head group, let nbs = map (\(_, w, c) -> (w, c)) group]
 
 
-toEdgeList :: Graph -> [Edge]
+toEdgeList :: FlowGraph -> [Edge]
 toEdgeList g = [(u, v, c) | (u, nbs) <- Map.assocs g, (v, c) <- Map.assocs nbs]
 
-getEdge :: Graph -> Vertex -> Vertex -> Edge
+getEdge :: FlowGraph -> Vertex -> Vertex -> Edge
 getEdge g u v = (u, v, c) where
     c = (g Map.! u) Map.! v
 
 
-neighbors :: Graph -> Vertex -> [(Vertex, Capacity)]
+neighbors :: FlowGraph -> Vertex -> [(Vertex, Capacity)]
 neighbors g v
     | Map.member v g = Map.assocs (g Map.! v)
     | otherwise = []
 
 
-pushFlow :: Graph -> Vertex -> Vertex -> Flow -> Graph
+pushFlow :: FlowGraph -> Vertex -> Vertex -> Flow -> FlowGraph
 pushFlow g u v f = Map.adjust (Map.adjust (subtract f) v) u g' where
     g' = Map.adjust (Map.adjust (+ f) u) v g
 
 
-pushFlowLayered :: Graph -> Vertex -> Vertex -> Flow -> Graph
+pushFlowLayered :: FlowGraph -> Vertex -> Vertex -> Flow -> FlowGraph
 pushFlowLayered g u v f = Map.adjust (Map.adjust (\c -> c - f) v) u g
 
 
--- Assumes there are no cycles of size 2
-constructResidual :: Graph -> Graph
+-- Assumes there are no cycles of size 2, otherwise it may yield a wrong result
+constructResidual :: FlowGraph -> FlowGraph
 constructResidual g = fromEdgeList $ edges ++ map (\(u, v, c) -> (v, u, 0)) edges where
     edges = toEdgeList g
 
 
-removeEdge :: Graph -> Vertex -> Vertex -> Graph
+removeEdge :: FlowGraph -> Vertex -> Vertex -> FlowGraph
 removeEdge g u v = Map.adjust (Map.delete v) u g
 
 restorePath :: VertexMap Vertex -> Vertex -> Vertex -> Maybe Path
@@ -65,7 +65,7 @@ restorePath parents s t
     | not $ Map.member t parents = Nothing
     | otherwise = (t:) <$> restorePath parents s (parents Map.! t)
 
--- reads a graph from a file in the format:
+-- reads a FlowGraph from a file in the format:
 -- n s t -- number of edges, source and sink on the first line
 -- u v c -- edge from u to v with capacity c on the remaining lines
 readFileEdgeList :: String -> IO (Vertex, Vertex, [Edge])
